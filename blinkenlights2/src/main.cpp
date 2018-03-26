9ca65f9ba67b8cbfe9cebf3a109d4f5be8ca9281
Jens Reimann <jreimann@redhat.com>
2018-03-26 16:32:17 +0200

    add two new examples

---
+++ blinkenlights2/src/main.cpp
@@ -0,0 +1,52 @@
+/********************************************************************************
+* Copyright (c) 2017 Red Hat Inc and others
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0
+********************************************************************************/
+
+#include <Arduino.h>
+#include <SPI.h>
+
+#define LED1 D1
+#define LED2 D2
+#define BUTTON D6
+#define LED3 D5
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println();
+
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+
+  pinMode(BUTTON, INPUT);
+
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+
+  Serial.println("Los geht's");
+}
+
+void loop() {
+
+  bool clicked = digitalRead(BUTTON);
+  if ( clicked ) {
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+  } else {
+    digitalWrite(LED2, HIGH);
+  }
+
+  delay(500);
+  digitalWrite(LED1, LOW);
+  delay(500);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED3, HIGH);
+}
